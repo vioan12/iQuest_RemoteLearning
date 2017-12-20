2b1e2b1dee5a7b236a594baa2cc19f9e53f6d25c
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2017-12-20 04:52:04 +0200

    Add Tema3

---
+++ VendingMachine/Product.cs
@@ -0,0 +1,17 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class Product
+    {
+        public string name { get; set; }
+        public ProductCategory category { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+        public int size { get; set; }
+    }
+}
---
+++ VendingMachine/ProductCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class ProductCollection
+    {
+        private List<Product> products_list;
+        public ProductCollection()
+        {
+            products_list = new List<Product>();
+        }
+        public void Add(Product Product)
+        {
+            products_list.Add(Product);
+        }
+        public void Remove(Product Product)
+        {
+            for (int i = 0; i < products_list.Count; i++)
+                if (products_list.ElementAt(i).name == Product.name && products_list.ElementAt(i).category.name == Product.category.name && products_list.ElementAt(i).price == Product.price && products_list.ElementAt(i).quantity == Product.quantity && products_list.ElementAt(i).size == Product.size)
+                {
+                    products_list.Remove(products_list.ElementAt(i));
+                }
+        }
+        public int Count()
+        {
+            return products_list.Count;
+        }
+        public Product GetItem(int index)
+        {
+            return products_list.ElementAt(index);
+        }
+    }
+}
