528369dc49d7ea3a4cb0ce82d0e21f1720c5ca1a
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2017-12-20 07:11:38 +0200

    Add Tema4

---
+++ VendingMachine/ContainableItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class ContainableItem
+    {
+        Dictionary<string, Product> products_list;
+            
+        public ContainableItem()
+        {
+            products_list = new Dictionary<string, Product>();
+        }
+        public void Add(Product Product, int row, int column)
+        {
+            if (!products_list.ContainsKey(Convert.ToString(row) + " " + Convert.ToString(column)))
+            {
+                products_list.Add(Convert.ToString(row) + " " + Convert.ToString(column), Product);
+            }
+        }
+        public void Remove(int row, int column)
+        {
+            if (products_list.ContainsKey(Convert.ToString(row) + " " + Convert.ToString(column)))
+            {
+                products_list.Remove(Convert.ToString(row) + " " + Convert.ToString(column));
+            }
+            
+        }
+        public int Count()
+        {
+            return products_list.Count;
+        }
+        public Product GetItem(int row, int column)
+        {
+            if (products_list.ContainsKey(Convert.ToString(row) + " " + Convert.ToString(column)))
+            {
+                return products_list[Convert.ToString(row) + " " + Convert.ToString(column)];
+            }
+            else
+                return null;
+        }
+        public Dictionary<string, Product>.ValueCollection GetAllItem()
+        {
+            return products_list.Values;
+        }
+    }
+}
